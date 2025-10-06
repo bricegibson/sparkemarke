bfddc88af25ca2be193b9e4d45aca375488378ad
Brice Gibson <brice@bricegibson.com>
2025-10-05 19:41:33 -0600

    2025.10.05 v6 clean students.db for new instance.

--- database.js
+++ database.js
@@ -9,28 +9,12 @@ console.log("✅ Connected to SQLite database (better-sqlite3).");
 // Optional: enforce foreign keys if you use them
 db.pragma("foreign_keys = ON");
 
-  db.prepare(`
-    drop table if exists schools;
-    CREATE TABLE IF NOT EXISTS schools (
-      schoolID TEXT PRIMARY KEY,
-      schoolName TEXT NOT NULL
-    )
-  `).run();
-
-  // Teachers table
-  db.prepare(`
-    drop table if exists teachers;
-    CREATE TABLE IF NOT EXISTS teachers (
-      teacherID TEXT PRIMARY KEY,
-      teacherName TEXT NOT NULL,
-      teacherPassword TEXT NULL,
-      teacherSchoolID TEXT NOT NULL
-    )
-  `).run();
-
   // Students table
   db.prepare(`
-    drop table if exists students;
+    drop table if exists students
+  `).run(); 
+
+  db.prepare(`
     CREATE TABLE IF NOT EXISTS students (
       studentID TEXT PRIMARY KEY,
       studentName TEXT NOT NULL,
@@ -38,19 +22,12 @@ db.pragma("foreign_keys = ON");
     )
   `).run();
 
-  // Subjects table (optional, could also just store in scores)
-  db.prepare(`
-    drop table if exists subjects;
-    CREATE TABLE IF NOT EXISTS subjects (
-      subjectID INTEGER PRIMARY KEY AUTOINCREMENT,
-      subjectTeacherID TEXT NOT NULL,
-      subjectName TEXT NOT NULL
-    )
-  `).run();
-
   // Scores table
   db.prepare(`
-    drop table if exists scores;
+    drop table if exists scores
+  `).run(); 
+
+  db.prepare(`
     CREATE TABLE IF NOT EXISTS scores (
       scoreID INTEGER PRIMARY KEY AUTOINCREMENT,
       scoreStudentID TEXT NOT NULL,
@@ -64,17 +41,10 @@ db.pragma("foreign_keys = ON");
   `).run();
 
     db.prepare(`
-      drop table if exists codes;
-    CREATE TABLE IF NOT EXISTS codes (
-        codeID INTEGER PRIMARY KEY AUTOINCREMENT,
-        codeTeacherID TEXT NOT NULL,
-        codeText TEXT NOT NULL,
-        codeCreatedDate DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-    `).run();  
+      drop table if exists studentGoals
+    `).run();    
 
     db.prepare(`
-      drop table if exists studentGoals;
       CREATE TABLE IF NOT EXISTS studentGoals (
         studentID TEXT NOT NULL,
         studentSubjectID INTEGER NOT NULL,
