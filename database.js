cf8f310efa6ad62012af3f64f579a9ae9e1b2f62
Brice Gibson <brice@bricegibson.com>
2025-10-05 20:45:48 -0600

    2025.10.05 v12 adding student goals table back in

--- database.js
+++ database.js
@@ -6,4 +6,14 @@ const db = new Database("/var/data/students.db");
 // Log a confirmation message once opened
 console.log("✅ Connected to SQLite database (better-sqlite3).");
 
+
+    db.prepare(`
+      CREATE TABLE IF NOT EXISTS studentGoals (
+        studentID TEXT NOT NULL,
+        studentSubjectID INTEGER NOT NULL,
+        studentGoal REAL NOT NULL,
+        PRIMARY KEY (studentID, studentSubjectID)
+      )
+    `).run();
+    
 module.exports = db;
